62a7187f4d50c5ce4d3e6864e1e79194d9e4d369
AnyOpenGL <AnyOpenGL@outlook.com>
2025-04-12 19:29:45 +0800

    finish easyPearl and improve codes

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleEasyPearl.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleEasyPearl.kt
@@ -1,28 +1,47 @@
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
+import net.ccbluex.liquidbounce.config.types.ToggleableConfigurable
 import net.ccbluex.liquidbounce.event.events.InteractItemEvent
 import net.ccbluex.liquidbounce.event.events.RotationUpdateEvent
+import net.ccbluex.liquidbounce.event.events.WorldRenderEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
+import net.ccbluex.liquidbounce.render.FULL_BOX
+import net.ccbluex.liquidbounce.render.drawGradientSides
+import net.ccbluex.liquidbounce.render.drawOutlinedBox
+import net.ccbluex.liquidbounce.render.engine.Color4b
+import net.ccbluex.liquidbounce.render.renderEnvironmentForWorld
+import net.ccbluex.liquidbounce.render.withColor
+import net.ccbluex.liquidbounce.render.withDisabledCull
+import net.ccbluex.liquidbounce.render.withPositionRelativeToCamera
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.RotationsConfigurable
 import net.ccbluex.liquidbounce.utils.aiming.data.Rotation
 import net.ccbluex.liquidbounce.utils.aiming.projectiles.SituationalProjectileAngleCalculator
+import net.ccbluex.liquidbounce.utils.block.getState
+import net.ccbluex.liquidbounce.utils.entity.ConstantPositionExtrapolation
+import net.ccbluex.liquidbounce.utils.entity.SimulatedPlayer
 import net.ccbluex.liquidbounce.utils.inventory.HotbarItemSlot
 import net.ccbluex.liquidbounce.utils.inventory.Slots
 import net.ccbluex.liquidbounce.utils.inventory.useHotbarSlotOrOffhand
 import net.ccbluex.liquidbounce.utils.kotlin.Priority
+import net.ccbluex.liquidbounce.utils.math.toBlockPos
+import net.ccbluex.liquidbounce.utils.math.toVec3d
 import net.ccbluex.liquidbounce.utils.render.trajectory.TrajectoryInfo
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.item.Items
 import net.minecraft.util.math.Vec3d
 
+
+@Suppress("MagicNumber")
 object ModuleEasyPearl : ClientModule("EasyPearl", Category.MISC) {
-    // Settings
     private val aimOffThreshold by float("AimOffThreshold", 2f, 0.5f..10f)
-    private val maxDistance by float("MaxDistance", 10f, 0.5f..100f)
+
+    private object Predict : ToggleableConfigurable(this, "Predict", true) {
+        val predictTicks by int("PredictTicks", 1, 1..5)
+    }
 
     private val rotation = tree(RotationsConfigurable(this))
     private var targetPosition: Vec3d? = null
@@ -31,18 +50,24 @@ object ModuleEasyPearl : ClientModule("EasyPearl", Category.MISC) {
     private val enderPearlSlot: HotbarItemSlot?
         get() = Slots.OffhandWithHotbar.findSlot(Items.ENDER_PEARL)
 
+    init {
+        tree(Predict)
+    }
+
     @Suppress("unused")
     private val interactItemHandler = handler<InteractItemEvent> { event ->
         if (player.inventory.mainHandStack.item != Items.ENDER_PEARL || !mc.options.useKey.isPressed) {
             return@handler
         }
 
-        if (isThrow) {
-            isThrow = false
-            return@handler
+        if (targetPosition != null) {
+            if (isThrow && isRotationDone(targetPosition!!)) {
+                isThrow = false
+                return@handler
+            }
         }
 
-        targetPosition = getPositionPlayerLookAt() ?: return@handler
+        targetPosition = getPositionPlayerLookAt()
 
         if (isRotationDone(targetPosition!!)) {
             targetPosition = null
@@ -52,7 +77,7 @@ object ModuleEasyPearl : ClientModule("EasyPearl", Category.MISC) {
     }
 
     @Suppress("unused")
-    private val onRotation = handler<RotationUpdateEvent> {
+    private val rotationHandler = handler<RotationUpdateEvent> {
         val currentTargetPosition = targetPosition ?: return@handler
         val finalTargetRotation = getTargetRotation(currentTargetPosition) ?: return@handler
 
@@ -64,7 +89,7 @@ object ModuleEasyPearl : ClientModule("EasyPearl", Category.MISC) {
     }
 
     @Suppress("unused")
-    private val onTick = tickHandler {
+    private val tickHandler = tickHandler {
         val currentPosition = targetPosition ?: return@tickHandler
         val currentTargetRotation = getTargetRotation(currentPosition) ?: return@tickHandler
         val slot = enderPearlSlot ?: return@tickHandler
@@ -76,6 +101,38 @@ object ModuleEasyPearl : ClientModule("EasyPearl", Category.MISC) {
         }
     }
 
+    @Suppress("unused")
+    private val worldRenderHandler = handler<WorldRenderEvent> { event ->
+        val matrixStack = event.matrixStack
+        val position = getPositionPlayerLookAt()
+        val blockPos = position.toBlockPos()
+        val state = blockPos.getState() ?: return@handler
+
+        renderEnvironmentForWorld(matrixStack) {
+            withDisabledCull {
+                val color = if (getTargetRotation(getPositionPlayerLookAt()) != null) {
+                    Color4b(0x20, 0xC2, 0x06)
+                } else {
+                    Color4b(0xD7, 0x09, 0x09)
+                }
+
+                val baseColor = color.with(a = 50)
+                val transparentColor = baseColor.with(a = 0)
+                val outlineColor = color.with(a = 100)
+
+                withPositionRelativeToCamera(blockPos.toVec3d()) {
+                    withColor(baseColor) {
+                        drawOutlinedBox(FULL_BOX)
+                    }
+                    withColor(outlineColor) {
+                        drawOutlinedBox(FULL_BOX)
+                    }
+                    drawGradientSides(1.0, baseColor, transparentColor, FULL_BOX)
+                }
+            }
+        }
+    }
+
     @Suppress("ReturnCount")
     private fun isRotationDone(targetPosition: Vec3d): Boolean {
         val currentTargetRotation = getTargetRotation(targetPosition) ?: return true
@@ -83,13 +140,26 @@ object ModuleEasyPearl : ClientModule("EasyPearl", Category.MISC) {
         return rotationDifference <= aimOffThreshold
     }
 
-    private fun getPositionPlayerLookAt(): Vec3d? =
-        player.raycast(maxDistance.toDouble(), 0.0f, false).pos
+    private fun getPositionPlayerLookAt(): Vec3d = player.raycast(1000.0, 0.0f, false).pos
 
-    private fun getTargetRotation(targetPosition: Vec3d): Rotation? =
-        SituationalProjectileAngleCalculator.calculateAngleForStaticTarget(
+    private fun getTargetRotation(targetPosition: Vec3d): Rotation? {
+        if (Predict.enabled) {
+            val nextTickPlayer =
+                SimulatedPlayer.fromClientPlayer(SimulatedPlayer.SimulatedPlayerInput.guessInput(player))
+            repeat(Predict.predictTicks - 1) {
+                nextTickPlayer.tick()
+            }
+            return SituationalProjectileAngleCalculator.calculateAngleFor(
+                TrajectoryInfo.GENERIC,
+                sourcePos = nextTickPlayer.pos,
+                targetPosFunction = ConstantPositionExtrapolation(targetPosition),
+                targetShape = EntityDimensions.fixed(1.0F, 0.0F)
+            )
+        }
+        return SituationalProjectileAngleCalculator.calculateAngleForStaticTarget(
             TrajectoryInfo.GENERIC,
             targetPosition,
             EntityDimensions.fixed(1.0F, 0.0F)
         )
+    }
 }
