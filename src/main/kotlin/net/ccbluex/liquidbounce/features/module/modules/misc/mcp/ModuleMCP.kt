cbf19f446de07f057f658fee80f8fede3e0b044c
AnyOpenGL <AnyOpenGL@outlook.com>
2025-06-01 18:27:25 +0800

    update

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/mcp/ModuleMCP.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/mcp/ModuleMCP.kt
@@ -10,7 +10,6 @@ import io.ktor.server.routing.routing
 import io.ktor.server.sse.SSE
 import io.ktor.server.sse.sse
 import io.ktor.util.collections.ConcurrentMap
-import io.modelcontextprotocol.kotlin.sdk.CallToolResult
 import io.modelcontextprotocol.kotlin.sdk.GetPromptResult
 import io.modelcontextprotocol.kotlin.sdk.Implementation
 import io.modelcontextprotocol.kotlin.sdk.PromptArgument
@@ -20,7 +19,6 @@ import io.modelcontextprotocol.kotlin.sdk.Role
 import io.modelcontextprotocol.kotlin.sdk.ServerCapabilities
 import io.modelcontextprotocol.kotlin.sdk.TextContent
 import io.modelcontextprotocol.kotlin.sdk.TextResourceContents
-import io.modelcontextprotocol.kotlin.sdk.Tool
 import io.modelcontextprotocol.kotlin.sdk.server.Server
 import io.modelcontextprotocol.kotlin.sdk.server.ServerOptions
 import io.modelcontextprotocol.kotlin.sdk.server.SseServerTransport
@@ -32,13 +30,20 @@ import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
-import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.features.module.modules.misc.mcp.features.tools.MCPToolChatWithClient
+import net.ccbluex.liquidbounce.features.module.modules.misc.mcp.features.tools.MCPToolSendServerMessage
 
 object ModuleMCP : ClientModule("MCP", Category.MISC) {
-    private val jsonObjectFormat =
+    val jsonObjectFormat =
         buildJsonObject {
             put("message", "")
         }
+
+    val mcpToolsList =
+        listOf<MCPFactory>(
+            MCPToolChatWithClient,
+            MCPToolSendServerMessage,
+        )
 
     override fun enable() {
         CoroutineScope(Dispatchers.Default).launch {
@@ -91,45 +96,8 @@ object ModuleMCP : ClientModule("MCP", Category.MISC) {
             )
         }
 
-        server.addTool(
-            name = "Chat with client",
-            description = "Chat with client",
-            inputSchema = Tool.Input(jsonObjectFormat),
-        ) { request ->
-
-            val message = request.arguments.get("message")?.toString() ?: ""
-
-            val callToolResult: MutableList<TextContent> = mutableListOf()
-            if (message.equals("")) {
-                callToolResult.add(TextContent("Empty message,please send again"))
-            } else {
-                callToolResult.add(TextContent("Message send successfully."))
-            }
-            chat(message)
-            CallToolResult(
-                content = listOf(TextContent("Message send successfully.")),
-            )
-        }
-
-        server.addTool(
-            name = "Send message to server",
-            description = "Send message to server,then other players can see what you say",
-            inputSchema = Tool.Input(jsonObjectFormat),
-        ) { request ->
-
-            var message = request.arguments.get("message")?.toString() ?: ""
-
-            message = message.removeSurrounding("\"")
-            val callToolResult: MutableList<TextContent> = mutableListOf()
-            if (message.equals("")) {
-                callToolResult.add(TextContent("Empty message,please send again"))
-            } else {
-                callToolResult.add(TextContent("Message send successfully."))
-            }
-            network.sendChatMessage(message)
-            CallToolResult(
-                content = listOf(TextContent("Message send successfully.")),
-            )
+        mcpToolsList.forEach {
+            it.addTool(server)
         }
 
         // Add a resource
