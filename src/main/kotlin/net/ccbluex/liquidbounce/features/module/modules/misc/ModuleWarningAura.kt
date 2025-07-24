e7cc101c843a05340fedc2bf83014bbfbe5a583a
AnyOpenGL <202985480+AnyOpenGL@users.noreply.github.com>
2025-07-24 10:20:45 +0800

    add header

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleWarningAura.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleWarningAura.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
+ *
+ * Copyright (c) 2015 - 2025 CCBlueX
+ *
+ * LiquidBounce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LiquidBounce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.event.events.OverlayRenderEvent
@@ -44,7 +62,7 @@ object ModuleWarningAura : ClientModule("WarningAura", Category.MISC) {
     @Suppress("unused")
     private val renderHandler = handler<OverlayRenderEvent> { event ->
         if (!needWarning || targetDistance == null) return@handler
-        val text = translation("liquidbounce.module.warningaura.warning").string + targetDistance
+        val text = translation("liquidbounce.module.warningAura.warning").string + targetDistance
         event.context.drawText(textRenderer,
             text,
             (event.context.scaledWindowWidth - textRenderer.getWidth(text)) / 2,
