9fb240f60077773555a8404ec5d89cf6dd1ab9b4
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-08-31 03:53:15 +0800

    cleanup

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleWarningAura.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/world/ModuleWarningAura.kt
@@ -16,57 +16,65 @@
  * You should have received a copy of the GNU General Public License
  * along with LiquidBounce. If not, see <https://www.gnu.org/licenses/>.
  */
-package net.ccbluex.liquidbounce.features.module.modules.misc
 
+package net.ccbluex.liquidbounce.features.module.modules.world
+
+import net.ccbluex.liquidbounce.event.events.GameTickEvent
 import net.ccbluex.liquidbounce.event.events.OverlayRenderEvent
 import net.ccbluex.liquidbounce.event.handler
-import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.lang.translation
 import net.ccbluex.liquidbounce.utils.combat.TargetTracker
+import net.ccbluex.liquidbounce.utils.combat.getEntitiesInCuboid
+import net.ccbluex.liquidbounce.utils.math.toFixed
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.Colors
 
-object ModuleWarningAura : ClientModule("WarningAura", Category.MISC, disableOnQuit = true) {
+object ModuleWarningAura : ClientModule("WarningAura", Category.WORLD, disableOnQuit = true) {
 
     private val onlyUnseen by boolean("OnlyUnseen", false)
     private val onlyPlayers by boolean("OnlyPlayers", false)
     private val autoDisconnect by boolean("AutoDisconnect", false)
     private val distance by float("Distance", 4.0f, 1.0f..6.0f)
 
-    private val targetTracker = TargetTracker()
+    private val targetTracker = tree(TargetTracker())
 
-    private var needWarning = false
     private var targetDistance: Float? = null
 
-    @Suppress("unused")
-    private val tickHandler = tickHandler {
-        val entities = world.entities.filter { it.distanceTo(player) < distance && it != player }
-            .filter { if (onlyPlayers) it is PlayerEntity else it is LivingEntity }
-            .filter { if (onlyUnseen) !player.canSee(it) else true }
-            .sortedBy { it.distanceTo(player) }
-
-        entities.forEach {
-            if (targetTracker.validate(it as LivingEntity)) {
-                if (autoDisconnect) {
-                    world.disconnect()
-                }
-                needWarning = true
-                targetDistance = it.distanceTo(player)
-                return@tickHandler
-            }
-        }
-
-        needWarning = false
+    override fun onDisabled() {
         targetDistance = null
     }
 
     @Suppress("unused")
+    private val tickHandler = handler<GameTickEvent> {
+        val entities = world.getEntitiesInCuboid(player.eyePos, distance.toDouble()) {
+            it !== player && (if (onlyPlayers) it is PlayerEntity else it is LivingEntity)
+                && (!onlyUnseen || player.canSee(it))
+        }
+        entities.sortBy { player.squaredDistanceTo(it) }
+
+        val closestEntity = entities.firstOrNull {
+            targetTracker.validate(it as LivingEntity)
+        }
+
+        if (closestEntity == null) {
+            targetDistance = null
+        } else {
+            if (autoDisconnect) {
+                world.disconnect()
+            }
+            targetDistance = closestEntity.distanceTo(player)
+        }
+    }
+
+    @Suppress("unused")
     private val renderHandler = handler<OverlayRenderEvent> { event ->
-        if (!needWarning || targetDistance == null) return@handler
-        val text = translation("liquidbounce.module.warningAura.warning").string + targetDistance
+        val targetDistance = targetDistance ?: return@handler
+
+        val text = translation("liquidbounce.module.warningAura.warning")
+            .append(targetDistance.toFixed(1))
         event.context.drawText(textRenderer,
             text,
             (event.context.scaledWindowWidth - textRenderer.getWidth(text)) / 2,
